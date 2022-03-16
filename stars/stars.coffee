72ab305d68bfd67dacc3b2a69f4dd19d5e0260d3
Alexey Yuchenko <alexes.dev@gmail.com>
2022-03-16 18:14:46 +0300

    initial commit

---
+++ stars/stars.coffee
@@ -0,0 +1,5 @@
+run = () ->
+  document.querySelector('#app').innerHTML = '<strong>hello</strong>'
+  console.log('hello from coffee')
+
+run()
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import CoffeeScript from 'coffeescript';
+
+const transformCS = {
+  name: 'coffee',
+  transform: (src, id) => {
+    console.log(id);
+    if (id.endsWith('.coffee')) {
+      const { js, sourceMap } = CoffeeScript.compile(src, { sourceMap: 'on' });
+      return { code: js, sourceMap };
+    }
+  },
+};
+
+export default defineConfig({
+  plugins: [transformCS],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        stars: resolve(__dirname, 'stars/index.html'),
+      }
+    }
+  },
+});
